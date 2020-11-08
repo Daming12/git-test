0b8e857461b4e6e651463da6e35a5f2ca06dac15
liujm <liujm@bingosoft.net>
2020-11-08 09:50:16 +0800

    修改！

--- src/com/dm/git/FirstClass.java
+++ src/com/dm/git/FirstClass.java
@@ -8,8 +8,12 @@ public class FirstClass {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println(123);
     }
     public void test(){
+
+        int a=1;
+        System.out.println(a);
         System.out.println("tttt");
     }
 }
